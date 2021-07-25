c5f9a40c194500cf620f82f2cb16079f31857fc3
hasbynatawijaya <mhasbyn@gmail.com>
2021-07-25 08:33:04 +0700

    initial commit

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
+
+import { accomplishmentData as data } from "../../constants/constants";
+
+const Acomplishments = () => (
+  <Section>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Personal Accomplishment</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
+);
+
+export default Acomplishments;
